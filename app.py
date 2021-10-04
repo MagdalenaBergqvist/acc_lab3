bb5a78795c3213bf71770b1359906fc21b921f8a
MagdalenaBergqvist <magdabergqvist@hotmail.com>
2021-10-04 21:15:53 +0200

    tries to make dumps work

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 @app.route('/')
 def welcolme():
     r = tasks.func.delay()
-    return json.dump(r.wait(), indent=4)
+    return json.dumps(r.wait(), indent=4)
 if __name__ == '__main__':
     app.run(host='0.0.0.0')#, port=5000)
