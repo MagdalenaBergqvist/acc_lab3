915fd808332ce977ca68742099d085cb1c1cd785
MagdalenaBergqvist <magdabergqvist@hotmail.com>
2021-10-04 14:50:06 +0200

    adding .wait

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def welcolme():
     r = tasks.func.delay()
-    return r
+    return r.wait()
     #return "Hello World!"
 if __name__ == '__main__':
     app.run(host='0.0.0.0')#, port=5000)
