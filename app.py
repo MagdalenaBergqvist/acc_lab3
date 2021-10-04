67229ea7ccd0fe8d15d0da4d7592dbdfc7e6ee45
MagdalenaBergqvist <magdabergqvist@hotmail.com>
2021-10-04 14:47:01 +0200

    using func

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
+import tasks
 app = Flask(__name__)
 @app.route('/')
 def welcolme():
-    return "Hello World!"
+    r = tasks.func.delay()
+    return r
+    #return "Hello World!"
 if __name__ == '__main__':
     app.run(host='0.0.0.0')#, port=5000)
