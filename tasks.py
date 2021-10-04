49ed71c8c8c700f596026cef548c68f3ad038acb
MagdalenaBergqvist <magdabergqvist@hotmail.com>
2021-10-04 15:07:42 +0200

    eg

--- tasks.py
+++ tasks.py
@@ -2,7 +2,7 @@ from celery import Celery
 from extract import count_pronouns
 
 app = Celery('tasks', backend='rpc://', broker='pyamqp://guest@localhost//')
-
+print("eogrb")
 @app.task
 def func():
     print("tasks")
